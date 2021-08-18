09ed1aa14b20abfbdea3cddf0bb82b5e51ae9dae
glendadrew <gadrew@ucdavis.edu>
2021-08-18 16:29:12 -0700

    udpate font link https

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-(function () {
+(function() {
     "use strict";
     const mainTag = document.querySelector('main');
     const bodyTag = document.querySelector('body');
-    const welcomeParticipateBtn =  document.querySelector('#participate-btn');
+    const welcomeParticipateBtn = document.querySelector('#participate-btn');
     const welcomeViewBtn = document.querySelector('#view-btn');
     const navBtn = document.querySelector('#nav');
     const navMenu = document.querySelector('header nav');
     const headerTag = document.querySelector('header');
     const homeIsLogo = document.querySelector('header a img')
     const headerDiv = document.querySelector('#container > div');
- 
+
     const tileImages = document.querySelectorAll('main img');
     const tileOverlay = document.querySelector('#tile-overlay');
     const tileImg = document.querySelector("#tile-overlay figure img");
@@ -23,9 +23,9 @@
     const containerDiv = document.querySelector('#container');
     const leftArrow = document.querySelectorAll('#tile-overlay > img')[0];
     const rightArrow = document.querySelectorAll('#tile-overlay > img')[1];
-    
+
     const teamOverlay = document.querySelector('#team-overlay');
-    const teamOverlaySection =  document.querySelector('#team-overlay section');
+    const teamOverlaySection = document.querySelector('#team-overlay section');
     const teamCloseBtn = document.querySelector('#team-close-btn');
     const teamImages = document.querySelectorAll('main figure img');
 
@@ -34,14 +34,14 @@
     // const navLis = document.querySelectorAll('nav li');
     /* const homeIsLink = navLis[0];
     const viewLink = navLis[1]; */
-   
+
 
     let imgTags;
     let imgInfo;
     let currentImage;
     let scrollValue = window.scrollY;
     /* let loaded = false; */
-    
+
     /* let headerHeight;
     let qrStickerHeight; */
 
@@ -87,27 +87,27 @@
 
     ]
 
-    
 
 
 
-    window.addEventListener('load', function(){
-         navBtn.addEventListener('click', showNavMenu);
+
+    window.addEventListener('load', function() {
+        navBtn.addEventListener('click', showNavMenu);
         /*  viewLink.addEventListener('click', function(){
              welcomeOverlay.className = 'view-display';
          })
          homeIsLink.addEventListener('click', function(){
             welcomeOverlay.className = 'landing-display';
         }) */
-       
-       
-        if (mainTag.className == 'tiles'){ /* TILES PAGE */
+
+
+        if (mainTag.className == 'tiles') { /* TILES PAGE */
 
             /* if(!loaded) {
                  welcomeOverlay.className = 'landing-display';
             } */
-            if(welcomeOverlay){
-                welcomeViewBtn.addEventListener('click', function(){
+            if (welcomeOverlay) {
+                welcomeViewBtn.addEventListener('click', function() {
                     location.href = "tiles.html";
                     const welcomeOverlay = document.querySelector('#welcome-overlay');
                     welcomeOverlay.style.display = 'none';
@@ -116,21 +116,23 @@
                     welcomeOverlay.className = 'view-display'; */
                 })
 
-                welcomeParticipateBtn.addEventListener('click', function(){
+                welcomeParticipateBtn.addEventListener('click', function() {
                     window.open("https://docs.google.com/forms/d/e/1FAIpQLSdSwkRE3-_cvD-W70YbJtvHuceNNj2xi8tbKbKpKnt-zUm_Pg/viewform", "_blank");
                 })
             }
 
-            leftArrow.addEventListener('click', function(){
+            leftArrow.addEventListener('click', function() {
                 currentImage--;
-                changePhoto()});
-        
-            rightArrow.addEventListener('click', function(){
-                currentImage++;
-                changePhoto()});
+                changePhoto()
+            });
 
-              /* Close tile overlay  */
-            closeBtn.addEventListener('click', function(){
+            rightArrow.addEventListener('click', function() {
+                currentImage++;
+                changePhoto()
+            });
+
+            /* Close tile overlay  */
+            closeBtn.addEventListener('click', function() {
                 tileOverlay.style.display = 'none';
                 bodyTag.style.overflow = 'initial';
             })
@@ -141,20 +143,20 @@
 
             imageEventListeners();
 
-        } else if (mainTag.className == 'team'){ /* TEAM */
+        } else if (mainTag.className == 'team') { /* TEAM */
             imgTags = teamImages;
             console.log(imgTags)
-   
-            teamCloseBtn.addEventListener('click', function(){
+
+            teamCloseBtn.addEventListener('click', function() {
                 teamOverlay.style.display = 'none';
                 bodyTag.style.overflow = 'initial';
             })
 
-            teamOverlay.addEventListener('click', function(){
+            teamOverlay.addEventListener('click', function() {
                 teamOverlay.style.display = 'none';
                 bodyTag.style.overflow = 'initial';
             })
-            teamOverlaySection.addEventListener('click', function(event){
+            teamOverlaySection.addEventListener('click', function(event) {
                 event.stopImmediatePropagation();
             })
             imageEventListeners();
@@ -164,8 +166,8 @@
 
 
     window.addEventListener('scroll', function() {
-    
-        if (window.scrollY > 125 && window.scrollY >= scrollValue){
+
+        if (window.scrollY > 125 && window.scrollY >= scrollValue) {
             headerTag.style.marginTop = '-15vh';
         } else {
             headerTag.style.marginTop = '0';
@@ -176,44 +178,44 @@
     /* Show Navigation menu */
     function showNavMenu() {
 
-        navMenu.style.display = 'flex'; 
+        navMenu.style.display = 'flex';
         headerDiv.style.backgroundColor = 'rgba(47, 47, 47, 0.6)';
         headerDiv.style.height = '100vh';
         headerTag.style.height = '100vh';
         containerDiv.style.overflow = 'hidden';
 
-        if(window.innerWidth < 1300){
-            headerTag.style.paddingTop = `calc((15vh - ${parseInt(homeIsLogo.height)}px) / 2`//'50px';     calc((15vh - 48px) / 2)
+        if (window.innerWidth < 1300) {
+            headerTag.style.paddingTop = `calc((15vh - ${parseInt(homeIsLogo.height)}px) / 2` //'50px';     calc((15vh - 48px) / 2)
             headerTag.style.alignContent = 'flex-start';
-           
+
 
         } else {
             headerTag.style.display = 'block';
             headerTag.style.width = '50vw';
             headerTag.style.backgroundColor = '#fdfdfd';
             headerTag.style.padding = '0';
-            
-            listElements.forEach(function(eachListEl){
-                eachListEl. style.fontSize = '3.2em';
+
+            listElements.forEach(function(eachListEl) {
+                eachListEl.style.fontSize = '3.2em';
             })
 
             navBtn.style.marginLeft = '0';
             navCloseBtn.style.display = 'block';
-    
+
         }
-       
+
         navBtn.removeEventListener('click', showNavMenu);
         navBtn.addEventListener('click', hideNavMenu);
         navCloseBtn.addEventListener('click', hideNavMenu);
 
-        setTimeout( function(){
+        setTimeout(function() {
             headerDiv.addEventListener('click', hideNavMenu);
-            headerTag.addEventListener('click', function(event){
+            headerTag.addEventListener('click', function(event) {
                 event.stopImmediatePropagation();
             })
         }, 500)
-       
-       
+
+
     }
 
     /* Remove Navigation menu */
@@ -223,7 +225,7 @@
         navBtn.addEventListener('click', showNavMenu);
         containerDiv.style.overflow = 'initial';
 
-        if(window.innerWidth < 1300){
+        if (window.innerWidth < 1300) {
             headerTag.style.height = '15vh';
             headerTag.style.paddingTop = '0';
             headerTag.style.alignContent = 'center';
@@ -232,7 +234,7 @@
             headerTag.style.backgroundColor = 'transparent';
             headerTag.style.width = '40%';
             headerTag.style.padding = '30px 0 0 30px';
-           
+            //    turn on hamburger menu
         }
 
         headerDiv.style.backgroundColor = 'transparent';
@@ -243,12 +245,12 @@
 
     }
 
-    function imageEventListeners(){
-        for (let i=0; i < imgTags.length; i++){
-           
-            imgTags[i].addEventListener('click', function(){
-                
-                if(mainTag.className == 'tiles'){
+    function imageEventListeners() {
+        for (let i = 0; i < imgTags.length; i++) {
+
+            imgTags[i].addEventListener('click', function() {
+
+                if (mainTag.className == 'tiles') {
                     showTileOverlay(imgTags[i], i);
                 } else {
                     showTeamOverlay(imgTags[i], i)
@@ -258,83 +260,83 @@
     }
 
     /* Show tile overlay */
-    function showTeamOverlay(img, num){
-     
+    function showTeamOverlay(img, num) {
+
         const teamImg = document.querySelector("#team-overlay section > img");
         let h3Tag = document.querySelector("#team-overlay section > h3");
         let pTag = document.querySelector("#team-overlay section > p");
-        
+
         teamImg.src = img.src;
         h3Tag.textContent = teamName[num]
         pTag.textContent = teamDescription[num]
 
-        if(num == 1){
+        if (num == 1) {
             pTag.appendChild(glendaLink);
 
-        } else if(num == 4){
+        } else if (num == 4) {
             pTag.appendChild(brettLink1);
             pTag.appendChild(brettText)
             pTag.appendChild(brettLink2);
         }
-        
+
         teamOverlay.style.display = 'block';
-      
+
         bodyTag.style.overflow = 'hidden';
-        
-      
+
+
     }
-    
+
     /* Show tile overlay */
-    function showTileOverlay(img, num){
-        if(window.innerWidth < 1300){
-            if(headerTag.style.marginTop == '0px' || window.scrollY < 125){
+    function showTileOverlay(img, num) {
+        if (window.innerWidth < 1300) {
+            if (headerTag.style.marginTop == '0px' || window.scrollY < 125) {
                 tileOverlay.style.top = '15vh';
             } else {
                 tileOverlay.style.top = '0';
             }
-        }   
+        }
         currentImage = num;
         tileImg.src = img.src;
         tileOverlay.style.display = 'flex';
         tileOverlay.style.height = window.innerHeight - headerTag.clientHeight;
         bodyTag.style.overflow = 'hidden';
-        
-      
+
+
     }
 
     /* Change photo to next with arrows */
-    function changePhoto(){
-        if(currentImage > (imgTags.length - 1)){
+    function changePhoto() {
+        if (currentImage > (imgTags.length - 1)) {
             currentImage = 0;
-        } else if(currentImage < 0){
+        } else if (currentImage < 0) {
             currentImage = imgTags.length - 1;
         }
 
         tileImg.src = imgTags[currentImage].src;
     }
-    
-   
-    function zoomIn(){
+
+
+    function zoomIn() {
         getImgInfo()
-   
-        if (window.innerWidth < 1300){
-            if(imgInfo.width >= window.innerWidth/2){
-                imgTags.forEach(function(eachImg){
+
+        if (window.innerWidth < 1300) {
+            if (imgInfo.width >= window.innerWidth / 2) {
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '100%';
                 })
-            } else if(imgInfo.width <= window.innerWidth/3){
-                imgTags.forEach(function(eachImg){
+            } else if (imgInfo.width <= window.innerWidth / 3) {
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '50%';
                 })
             }
         } else {
-            if(imgInfo.width <= window.innerWidth/7){
-                imgTags.forEach(function(eachImg){
+            if (imgInfo.width <= window.innerWidth / 7) {
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '16.66%';
                 })
                 qrSticker.style.width = '16.66%';
-            } else if(imgInfo.width <= window.innerWidth/6){
-                imgTags.forEach(function(eachImg){
+            } else if (imgInfo.width <= window.innerWidth / 6) {
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '20%';
                 })
                 qrSticker.style.width = '20%';
@@ -342,38 +344,38 @@
         }
     }
 
-    function zoomOut(){
+    function zoomOut() {
         getImgInfo()
-       
-        if (window.innerWidth < 1300){
-            if(imgInfo.width <= window.innerWidth/2){
+
+        if (window.innerWidth < 1300) {
+            if (imgInfo.width <= window.innerWidth / 2) {
                 console.log("in zoom out 33%")
-                imgTags.forEach(function(eachImg){
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '33.33%'
                 })
-            } else if(imgInfo.width = window.innerWidth){
-                imgTags.forEach(function(eachImg){
+            } else if (imgInfo.width = window.innerWidth) {
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '50%'
                 })
             }
-        } else{
-            if(imgInfo.width <= window.innerWidth/6){
-                imgTags.forEach(function(eachImg){
+        } else {
+            if (imgInfo.width <= window.innerWidth / 6) {
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '14.28%';
                 })
                 qrSticker.style.width = '14.28%';
-            } else if(imgInfo.width = window.innerWidth/5){
-                imgTags.forEach(function(eachImg){
+            } else if (imgInfo.width = window.innerWidth / 5) {
+                imgTags.forEach(function(eachImg) {
                     eachImg.style.width = '16.66%';
                 })
                 qrSticker.style.width = '16.66%';
-            } 
-        } 
+            }
+        }
 
     }
 
     /* Get image info to be able to use img width in zooming feature */
-    function getImgInfo(){
+    function getImgInfo() {
         imgInfo = imgTags[0].getBoundingClientRect();
     }
 
